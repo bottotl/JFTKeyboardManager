be988c6c272d29dd1169c46ae94be3fc476328fd
syfll <lintao.yu@dianping.com>
2017-12-22 16:21:00 +0800

    Initial Commit

---
+++ JFTKeyboardManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFTKeyboardManager
+//
+//  Created by syfll on 2017/12/22.
+//  Copyright © 2017年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
