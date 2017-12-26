0a3680786d43232d49c25c9fe4d36eab1261a97d
syfll <lintao.yu@dianping.com>
2017-12-26 18:06:23 +0800

    添加测试浮动输入框的页面

---
+++ JFTKeyboardManager/TestMessageStyleToolBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestMessageStyleToolBarViewController.h
+//  JFTKeyboardManager
+//
+//  Created by syfll on 2017/12/26.
+//  Copyright © 2017年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestMessageStyleToolBarViewController : UIViewController
+
+@end
---
+++ JFTKeyboardManager/TestMessageStyleToolBarViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestMessageStyleToolBarViewController.m
+//  JFTKeyboardManager
+//
+//  Created by syfll on 2017/12/26.
+//  Copyright © 2017年 syfll. All rights reserved.
+//
+
+#import "TestMessageStyleToolBarViewController.h"
+#import "JFTTestMessageStyleToolBar.h"
+
+@interface TestMessageStyleToolBarViewController ()
+@property (nonatomic, strong) JFTTestMessageStyleToolBar *jft_toolBar;
+@end
+
+@implementation TestMessageStyleToolBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.jft_toolBar = [[JFTTestMessageStyleToolBar alloc] initWithFrame:CGRectMake(0, 200, CGRectGetWidth(self.view.frame), 200)];
+    [self.view addSubview:self.jft_toolBar];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
