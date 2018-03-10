a55e21a2e2b8b3f4ce476bad254297dd39b7b902
syfll <lintao.yu@dianping.com>
2018-03-10 11:48:31 +0800

    回复误删的测试类

---
+++ JFTKeyboardManager/TestContentTextView.h
@@ -0,0 +1,13 @@
+//
+//  TestContentTextView.h
+//  JFTKeyboardManager
+//
+//  Created by syfll on 2018/3/10.
+//  Copyright © 2018年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestContentTextView : UITextView
+
+@end
