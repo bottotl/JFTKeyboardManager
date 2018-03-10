672f6e852e8ddd00d37c2b411ef00223094b09ab
syfll <lintao.yu@dianping.com>
2018-03-10 22:32:51 +0800

    add viewcontroller push test

---
+++ JFTKeyboardManager/JFTViewControllerA.h
@@ -0,0 +1,13 @@
+//
+//  JFTViewControllerA.h
+//  JFTKeyboardManager
+//
+//  Created by syfll on 2018/3/10.
+//  Copyright © 2018年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFTViewControllerA : UIViewController
+
+@end
---
+++ JFTKeyboardManager/JFTViewControllerA.m
@@ -0,0 +1,31 @@
+//
+//  JFTViewControllerA.m
+//  JFTKeyboardManager
+//
+//  Created by syfll on 2018/3/10.
+//  Copyright © 2018年 syfll. All rights reserved.
+//
+
+#import "JFTViewControllerA.h"
+#import "UIResponder+JFTKeyboard.h"
+
+@interface JFTViewControllerA ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
+@implementation JFTViewControllerA
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.textView.jft_needInputAccessoryView = YES;
+    self.textView.jft_needAvoidKeyboardHide  = YES;
+    self.textView.jft_shouldResignOnTouchOutside = YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
