6dd10f3a23c4d89fb4d65a97a31b38dcbe862e52
syfll <lintao.yu@dianping.com>
2018-03-31 15:34:00 +0800

    工具类添加

---
+++ JFTKeyboardManager/Util/UIView+Layout.h
@@ -0,0 +1,22 @@
+//
+//  UIView+Layout.h
+//  JFTKeyboardManager
+//
+//  Created by syfll on 2018/3/31.
+//  Copyright © 2018年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Layout)
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@property (nonatomic) CGFloat centerX;
+@property (nonatomic) CGFloat centerY;
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize size;
+@end
